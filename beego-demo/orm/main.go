a4b30de6d7448b93e69ff009eff77ddcb9eff8a4
liangjisheng <1294851990@qq.com>
2019-01-29 18:58:56 +0800

    orm

---
+++ beego-demo/orm/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+
+	orm.RegisterDataBase("default", "mysql", "root:ljs199711@/orm_test?charset=utf8")
+}
+
+func main() {
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	profile := new(Profile)
+	profile.Age = 30
+
+	user := new(User)
+	user.Profile = profile
+	user.Name = "slene"
+
+	fmt.Println(o.Insert(profile))
+	fmt.Println(o.Insert(user))
+}
