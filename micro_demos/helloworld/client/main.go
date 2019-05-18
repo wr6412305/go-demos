2b7afcc31623ba24c0a3be68cfa45e71e4509b6b
liangjisheng <1294851990@qq.com>
2019-05-18 20:57:51 +0800

    client

--- micro_demos/helloworld/client/main.go
+++ micro_demos/helloworld/client/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	proto "go-demos/micro_demos/helloworld/proto"
+
+	"github.com/micro/go-micro"
+)
+
+func main() {
+	// Create a new service. Optionally include some options here.
+	service := micro.NewService(
+		micro.Name("greeter"),
+	)
+
+	// Init will parse the command line flags.
+	service.Init()
+
+	// Create new greeter client
+	greeter := proto.NewGreeterService("greeter", service.Client())
+
+	// Creater new greeter
+	ticker := time.NewTicker(time.Second)
+	for {
+		// Call the greeter
+		rsp, err := greeter.Hello(context.TODO(), &proto.HelloRequest{Name: "ljs"})
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(rsp.Greeting)
+		<-ticker.C
+	}
 }
