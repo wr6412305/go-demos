424e94abfb24e9a4636b00f43d114abfa00c4c3b
liangjisheng <1294851990@qq.com>
2020-03-24 22:58:33 +0800

    jaeger

---
+++ jaeger-demo/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/jaegertracing/jaeger/releases/download/v1.17.1/jaeger-1.17.1-linux-amd64.tar.gz
+tar zxf jaeger-1.17.1-linux-amd64.tar.gz
+cd jaeger-1.17.1-linux-amd64
