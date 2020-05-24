3564ae9b693138009f063a8a6a41974a77859407
liangjisheng <1294851990@qq.com>
2020-05-24 21:03:23 +0800

    fix grpc swagger

--- grpc-demo/grpc-example/10-grpc-gateway/proto/proto.sh
+++ grpc-demo/grpc-example/10-grpc-gateway/proto/proto.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# 生成http.pb.go
+# 编译api下的http.pb.go
 protoc --go_out=plugins=grpc:./ ./google/api/http.proto
 
-# 生成annotations.pb.go
+# 编译api下annotations.pb.go
 protoc --proto_path=. --go_out=plugins=grpc:./ ./google/api/annotations.proto
 
-# 生成simple.validator.pb.go和simple.pb.go
+# 编译options下openapiv2.proto
+protoc --proto_path=. --go_out=plugins=grpc:./ ./google/options/openapiv2.proto
+
+# 编译options下annotations.proto
+protoc --proto_path=. --go_out=plugins=grpc:./ ./google/options/annotations.proto
+
+# 编译simple.validator.pb.go和simple.pb.go
 protoc --proto_path=${GOPATH}/src --proto_path=. --govalidators_out=. --go_out=plugins=grpc:. ./simple.proto
 
-# 生成simple.pb.gw.go
+# 编译simple.pb.gw.go
 protoc --proto_path=${GOPATH}/src --proto_path=. --grpc-gateway_out=logtostderr=true:. ./simple.proto
 
-# 生成simple.swagger.json
+# 编译simple.swagger.json
 protoc --proto_path=${GOPATH}/src --proto_path=. --swagger_out=logtostderr=true:./ ./simple.proto
